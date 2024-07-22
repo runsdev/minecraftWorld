46eb1d4febe211274ba4ff60bcce8761722e029e
runs664 <harunsixsixfour@gmail.com>
2024-07-22 04:58:12 +0000

    save: Monday, 22 July 2024 : 11.58.12 WIB

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Check if the screen session named "minecraft" exists
 if ! screen -list | grep -q "minecraft"; then
     # If it doesn't exist, create a new screen session and start the Minecraft server
-    screen -S minecraft -dm java -Xms1G -Xmx4G -jar server1201.jar nogui
+    screen -S minecraft -dm java -Xms3G -Xmx4G -jar server1201.jar nogui
 else
     # If it exists, reattach to the existing screen session
     screen -d -r minecraft
